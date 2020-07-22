e7a5b8a443f8c123dd3f203c7cc259ba80fd07bc
Nabin Thomas <nabin@youknowwhere>
2020-07-21 20:34:21 -0700

    Initial commit with firebase messaging just working

---
+++ app/src/main/java/com/aze/homemonitor/NTFirebaseMessagingService.kt
@@ -0,0 +1,69 @@
+package com.aze.homemonitor
+
+import android.app.Service
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+
+
+class NTFirebaseMessagingService : FirebaseMessagingService() {
+    companion object {
+        private const val TAG = "NTFirebaseMessagingService"
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.from)
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.size > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+            if ( /* Check if data needs to be processed by long running job */true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+                scheduleJob()
+            } else {
+                // Handle message within 10 seconds
+                handleNow()
+            }
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.notification != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body)
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+    private fun handleNow() {
+        // TODO("Not yet implemented")
+    }
+
+    private fun scheduleJob() {
+        // TODO("Not yet implemented")
+    }
+
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+        super.onNewToken(token)
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        // sendRegistrationToServer(token)
+    }
+}
