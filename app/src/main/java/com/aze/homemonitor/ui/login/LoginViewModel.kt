ddfb2b5889704597ec42f65e16293b7c01ab08f7
nabinthomas <nabin.thomas@gmail,com>
2020-07-25 22:55:01 -0700

    more cleanups

--- app/src/main/java/com/aze/homemonitor/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/aze/homemonitor/ui/login/LoginViewModel.kt
@@ -3,16 +3,12 @@ package com.aze.homemonitor.ui.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import android.util.Patterns
-import com.aze.homemonitor.data.LoginRepository
-import com.aze.homemonitor.data.Result
 import androidx.lifecycle.map
 
 
-import com.aze.homemonitor.R
 import com.google.firebase.auth.FirebaseUser
 
-class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel() {
+class LoginViewModel() : ViewModel() {
 
     enum class AuthenticationState {
         AUTHENTICATED, UNAUTHENTICATED, INVALID_AUTHENTICATION
@@ -25,10 +21,6 @@ class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel()
             AuthenticationState.UNAUTHENTICATED
         }
     }
-
-
-    private val _loginForm = MutableLiveData<LoginFormState>()
-    val loginFormState: LiveData<LoginFormState> = _loginForm
 
     private val _loginResult = MutableLiveData<LoginResult>()
     val loginResult: LiveData<LoginResult> = _loginResult
