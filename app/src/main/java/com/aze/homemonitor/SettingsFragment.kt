9ab854cd7627b2ae39da37c23a85d7b7a58d7bc8
nabinthomas <nabin.thomas@gmail,com>
2020-07-27 21:30:33 -0700

    vars for UI elements

--- app/src/main/java/com/aze/homemonitor/SettingsFragment.kt
+++ app/src/main/java/com/aze/homemonitor/SettingsFragment.kt
@@ -5,8 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.Switch
+import android.widget.TextView
 
 class SettingsFragment : Fragment() {
+
+    var temperatureLabel : TextView? = null
+    var tempLowRange : SeekBar? = null
+    var tempHighRange : SeekBar? = null
+    var humidityLabel : TextView? = null
+    var humidityLowRange : SeekBar? = null
+    var humidityHighRange : SeekBar? = null
+    var motionSensorSwitch: Switch? = null
+    var soundSensorSwitch: Switch? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +29,9 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val rootview : View = inflater.inflate(R.layout.fragment_settings, container, false)
+
+        return rootview
     }
 
     companion object {
